68fec70155c567bc876b5a8fa615a376e6444db4
AbhishekKhursange <abhishekkhursange139@gmail.com>
2026-07-10 14:25:31 +0530

    JWT expiry time change

--- order-management-frontend/src/Home.jsx
+++ order-management-frontend/src/Home.jsx
@@ -165,10 +165,6 @@ function Home() {
         </div>
       </footer>
 
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
-      />
     </div>
   );
 }
